679c8468cfd3bfa0a262915abcb5cfe84d0f6440
Solomon Pierce <62899156+spierceVR@users.noreply.github.com>
2025-04-08 13:00:18 -0500

    add authorization to backend

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,21 @@
-import dotenv from 'dotenv';
-dotenv.config();
 import express, { json } from 'express';
 import cors from 'cors';
 import assignmentsRouter from './controllers/assignments.controller.js';
 import mongoose from 'mongoose';
 
-mongoose.connect('mongodb://mongo:27017/gas', {
+const username = process.env.MONGO_INITDB_ROOT_USERNAME;
+const pw = process.env.MONGO_INITDB_ROOT_PASSWORD;
+mongoose.connect(`mongodb://${username}:${pw}@mongo:27017/gas?authSource=${process.env.MONGO_DB_AUTH_SOURCE}`, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 mongoose.connection.once('open', () => {
-    console.log('✅ Connected to MongoDB');
-  });
-  
-  mongoose.connection.on('error', (err) => {
-    console.error('❌ MongoDB connection error:', err);
-  });
+  console.log('✅ Connected to MongoDB');
+});
+
+mongoose.connection.on('error', (err) => {
+  console.error('❌ MongoDB connection error:', err);
+});
 const app = express();
 app.use(cors());
 app.use(json());
@@ -26,9 +26,9 @@ app.use('/api/assignments', assignmentsRouter);
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
-    res.send('Backend is running!');
+  res.send('Backend is running!');
 });
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
