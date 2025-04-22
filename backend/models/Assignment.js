66726870e228698f95f88b44d7208e5830771a93
Rayyan Waris <raywa04@gmail.com>
2025-04-22 02:10:44 -0500

    fix: Assignment schema to match Section schema

--- backend/models/Assignment.js
+++ backend/models/Assignment.js
@@ -2,7 +2,7 @@ import { Schema, model } from 'mongoose';
 
 const assignmentSchema = new Schema({
   grader_id: { type: Schema.Types.ObjectId, ref: 'Candidate', required: true },
-  course_section_id: { type: Schema.Types.ObjectId, ref: 'Course', required: true },
+  course_section_id: { type: Schema.Types.ObjectId, ref: 'Section', required: true },
   status: { type: String, enum: ['pending', 'accepted'], default: 'pending' },
   semester: { type: String, required: true },
   score: { type: Number, default: 0 },
